824b9dc1af480b2c464953d1204668eecd253d4b
Carlos Andrade <ravens452@gmail.com>
2021-06-16 12:01:04 -0500

    1

---
+++ cmake-build-debug/CMakeFiles/github.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/github.dir/main.cpp.o"
+  "github"
+  "github.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
